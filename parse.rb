280031d861d1b89443c86c5648b443864f556e69
JScott <jvscott@gmail.com>
2014-05-14 16:58:31 -0400

    The first scene!
    Boxes and Planes are rendered
    Color, scale, position, and rotation are provided

--- parse.rb
+++ parse.rb
@@ -8,13 +8,58 @@ require 'yaml'
 
 #users = db['users'], etc
 
+default = {
+  :color => "0xffffff",
+  :scale => "1,1,1", # providing an extra param to planes...
+  :position => "0,0,0",
+  :rotation => "0,0,0"
+}
+
 mvml = YAML.load_file 'index.mvml'
 
 eruby = Erubis::Eruby.new File.read('index.eruby')
 
 template_vars = {}
 template_vars['title'] = mvml['title']
-template_vars['objects'] = {}
+template_vars['objects'] = []
+mvml['scene'].each do |object|
+  if object.has_key? 'model'
+    render_call = case object['model']
+    when 'box'
+      "BoxGeometry"
+    when 'plane' 
+      "PlaneGeometry"
+    else
+      # a model
+    end
+
+    rotation = default[:rotation]
+    if object.has_key? 'rotation'
+      rotation = object['rotation'].split ','
+      rotation.map! do |rotation| 
+        rotation.to_f * Math::PI / 180
+      end
+      rotation = rotation.join ','
+    end
+    puts rotation
+    
+    template_vars['objects'].push({
+      :render_call => render_call,
+      :color => object['color'] || default[:color],
+      :scale => object['scale'] || default[:scale],
+      :position => object['position'] || default[:position],
+      :rotation => rotation
+    })
+  end
+  if object.has_key? 'light'
+  end
+  if object.has_key? 'audio'
+  end
+end
 # + audio, lights
 
-puts eruby.result(template_vars)
+#puts eruby.result(template_vars)
+puts mvml
+File.open('index.html', 'w') { |file|
+  file.write eruby.result(template_vars)
+}
