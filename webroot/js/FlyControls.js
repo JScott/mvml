2b136b8434b92e8413183d6d94f45241c9ede279
JScott <jvscott@gmail.com>
2014-05-26 15:18:14 -0400

    Reverted inputs to flycontrols
    The calls were interfering and mousemove only happened when the mouse moved
    It's do-able but doesn't have enough value right now
    A card is made for later

--- webroot/js/FlyControls.js
+++ webroot/js/FlyControls.js
@@ -40,6 +40,76 @@ THREE.FlyControls = function ( object, domElement ) {
 
   };
 
+  this.keydown = function( event ) {
+
+    if ( event.altKey ) {
+
+      return;
+
+    }
+
+    //event.preventDefault();
+
+    switch ( event.keyCode ) {
+
+      case 16: /* shift */ this.movementSpeedMultiplier = .1; break;
+
+      case 87: /*W*/ this.moveState.forward = 1; break;
+      case 83: /*S*/ this.moveState.back = 1; break;
+
+      case 65: /*A*/ this.moveState.left = 1; break;
+      case 68: /*D*/ this.moveState.right = 1; break;
+
+      case 82: /*R*/ this.moveState.up = 1; break;
+      case 70: /*F*/ this.moveState.down = 1; break;
+
+      case 38: /*up*/ this.moveState.pitchUp = 1; break;
+      case 40: /*down*/ this.moveState.pitchDown = 1; break;
+
+      case 37: /*left*/ this.moveState.yawLeft = 1; break;
+      case 39: /*right*/ this.moveState.yawRight = 1; break;
+
+      case 81: /*Q*/ this.moveState.rollLeft = 1; break;
+      case 69: /*E*/ this.moveState.rollRight = 1; break;
+
+    }
+
+    this.updateMovementVector();
+    this.updateRotationVector();
+
+  };
+
+  this.keyup = function( event ) {
+
+    switch( event.keyCode ) {
+
+      case 16: /* shift */ this.movementSpeedMultiplier = 1; break;
+
+      case 87: /*W*/ this.moveState.forward = 0; break;
+      case 83: /*S*/ this.moveState.back = 0; break;
+
+      case 65: /*A*/ this.moveState.left = 0; break;
+      case 68: /*D*/ this.moveState.right = 0; break;
+
+      case 82: /*R*/ this.moveState.up = 0; break;
+      case 70: /*F*/ this.moveState.down = 0; break;
+
+      case 38: /*up*/ this.moveState.pitchUp = 0; break;
+      case 40: /*down*/ this.moveState.pitchDown = 0; break;
+
+      case 37: /*left*/ this.moveState.yawLeft = 0; break;
+      case 39: /*right*/ this.moveState.yawRight = 0; break;
+
+      case 81: /*Q*/ this.moveState.rollLeft = 0; break;
+      case 69: /*E*/ this.moveState.rollRight = 0; break;
+
+    }
+
+    this.updateMovementVector();
+    this.updateRotationVector();
+
+  };
+
   this.mousedown = function( event ) {
 
     if ( this.domElement !== document ) {
@@ -116,6 +186,7 @@ THREE.FlyControls = function ( object, domElement ) {
   };
 
   this.update = function( delta ) {
+
     var moveMult = delta * this.movementSpeed;
     var rotMult = delta * this.rollSpeed;
     var forward = this.lookVector.clone();
@@ -146,6 +217,7 @@ THREE.FlyControls = function ( object, domElement ) {
   };
 
   this.updateMovementVector = function() {
+
     var forward = ( this.moveState.forward || ( this.autoForward && !this.moveState.back ) ) ? 1 : 0;
 
     this.moveVector.x = ( -this.moveState.left    + this.moveState.right );
@@ -153,6 +225,7 @@ THREE.FlyControls = function ( object, domElement ) {
     this.moveVector.z = ( -forward + this.moveState.back );
 
     //console.log( 'move:', [ this.moveVector.x, this.moveVector.y, this.moveVector.z ] );
+
   };
 
   this.updateRotationVector = function() {
@@ -200,6 +273,9 @@ THREE.FlyControls = function ( object, domElement ) {
   this.domElement.addEventListener( 'mousemove', bind( this, this.mousemove ), false );
   this.domElement.addEventListener( 'mousedown', bind( this, this.mousedown ), false );
   this.domElement.addEventListener( 'mouseup',   bind( this, this.mouseup ), false );
+
+  window.addEventListener( 'keydown', bind( this, this.keydown ), false );
+  window.addEventListener( 'keyup',   bind( this, this.keyup ), false );
 
   this.updateMovementVector();
   this.updateRotationVector();
