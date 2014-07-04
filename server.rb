9471ce21d4336b7b4df33d3bd1630d5cae9c5005
JScott <jvscott@gmail.com>
2014-07-03 21:55:09 -0400

    POST-able
    New post path
    New default port

--- server.rb
+++ server.rb
@@ -4,10 +4,16 @@ require './mvml'
 
 set :public, 'webroot'
 set :bind, '0.0.0.0'
-set :port, ARGV[0] || 80
+set :port, ARGV[0] || 6865
 
 get '/' do
   MVML.to_html 'index.mvml'
+end
+
+post '/' do
+  request.body.rewind
+  # TODO: don't crash on error
+  MVML.to_html request.body.read
 end
 
 get '/raw' do
