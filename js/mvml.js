e2093ddb0a893689409bca3cb4f8cbc36e6d7532
JScott <jvscott@gmail.com>
2014-09-05 01:05:33 -0400

    Fixed the dependency problem (test vs real)

--- js/mvml.js
+++ js/mvml.js
@@ -1,8 +1,13 @@
 var scripts = [
-  'js/mvml-dependencies.js',
-  'js/mvml-api.js',
-  'js/mvml-replace.js'
+  '//cdnjs.cloudflare.com/ajax/libs/js-yaml/3.0.2/js-yaml.min.js',
+  '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js',
+  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
+  '//cdnjs.cloudflare.com/ajax/libs/loglevel/1.1.0/loglevel.min.js',
+  'js/mvml-api.js'
 ];
+if (typeof jasmine === "undefined") {
+  scripts.push('js/mvml-replace.js');
+}
 var head = document.getElementsByTagName('head')[0];
 for(var i = 0; i < scripts.length; i++) {
   var script = document.createElement('script');
