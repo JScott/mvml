ab56e1f4a2b4b3c52f3450522a72605d3edb9e79
JScott <jvscott@gmail.com>
2014-09-04 21:39:46 -0400

    Set up Jasmine for testing

---
+++ js/mvml-dependencies.js
@@ -0,0 +1,14 @@
+var scripts = [
+  '//cdnjs.cloudflare.com/ajax/libs/js-yaml/3.0.2/js-yaml.min.js',
+  '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js',
+  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
+  '//cdnjs.cloudflare.com/ajax/libs/loglevel/1.1.0/loglevel.min.js',
+];
+var head = document.getElementsByTagName('head')[0];
+for(var i = 0; i < scripts.length; i++) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = scripts[i];
+  script.async = false;
+  head.appendChild(script);
+}
--- js/mvml.js
+++ js/mvml.js
@@ -1,10 +1,7 @@
 var scripts = [
-  '//cdnjs.cloudflare.com/ajax/libs/js-yaml/3.0.2/js-yaml.min.js',
-  '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js',
-  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
-  '//cdnjs.cloudflare.com/ajax/libs/loglevel/1.1.0/loglevel.min.js',
+  'js/mvml-dependencies.js',
   'js/mvml-api.js',
-  'js/mvml-load.js'
+  'js/mvml-replace.js'
 ];
 var head = document.getElementsByTagName('head')[0];
 for(var i = 0; i < scripts.length; i++) {
