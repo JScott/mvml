985f3f677140abc060b7ab67337cb5f39740d8bf
JScott <jvscott@gmail.com>
2014-09-06 18:24:56 -0400

    These URLs are becoming a problem...

--- mvml-dist/js/mvml.js
+++ mvml-dist/js/mvml.js
@@ -3,10 +3,10 @@ var scripts = [
   '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js',
   '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
   '//cdnjs.cloudflare.com/ajax/libs/loglevel/1.1.0/loglevel.min.js',
-  'js/mvml-api.js'
+  './mvml-api.js'
 ];
 if (typeof mocha === "undefined") {
-  scripts.push('js/mvml-replace.js');
+  scripts.push('./mvml-replace.js');
 }
 var head = document.getElementsByTagName('head')[0];
 for(var i = 0; i < scripts.length; i++) {
