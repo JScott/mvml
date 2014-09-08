063b5838b9541db82206bec149ff0ecd0979ec4e
JScott <jvscott@gmail.com>
2014-09-07 20:01:45 -0400

    Fixed spec.html

--- js/mvml-load.js
+++ js/mvml-load.js
@@ -1,3 +1,5 @@
-var mvml = document.getElementsByTagName('mvml')[0].innerHTML;
-log.setLevel('trace');
-MVML.load_string(mvml);
+window.onload = function() {
+  var mvml = document.getElementsByTagName('mvml')[0].innerHTML;
+  log.setLevel('trace');
+  MVML.load_string(mvml);
+}
