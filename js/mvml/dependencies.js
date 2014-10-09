ac435e600d644f466e2051324e130898bb6c10c4
JScott <jvscott@gmail.com>
2014-10-09 00:06:05 -0400

    Moved spec MVMLs into docs directory. Set up interactions.mvml to set up hooks around

--- js/mvml/dependencies.js
+++ js/mvml/dependencies.js
@@ -4,7 +4,7 @@ var scripts = [
   '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
   '//cdnjs.cloudflare.com/ajax/libs/loglevel/1.1.0/loglevel.min.js',
   '//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.14/require.min.js',
-  'js/mvml/helpers.js'
+  '/js/mvml/helpers.js'
 ];
 var head = document.getElementsByTagName('head')[0];
 for(var i = 0; i < scripts.length; i++) {
