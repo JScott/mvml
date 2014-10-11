41f6c30438464f06614b96d1f0d6ed89b18e5579
JScott <jvscott@gmail.com>
2014-10-11 18:48:58 -0400

    events -> hooks. better syntax, same concept

--- js/mvml.js
+++ js/mvml.js
@@ -5,7 +5,7 @@ var scripts = [
   '//cdnjs.cloudflare.com/ajax/libs/loglevel/1.1.0/loglevel.min.js',
   '//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.14/require.min.js',
   '/js/mvml/helpers.js',
-  '/js/mvml/events.js',
+  '/js/mvml/hooks.js',
   '/js/mvml/api.js',
   '/js/mvml/load.js'
 ];
---
+++ js/mvml/hooks.js
@@ -0,0 +1,29 @@
+var MVML = MVML || {};
+_.extend(MVML, {
+  hook: {
+    list: {},
+
+    on: function(hook_name, callback) {
+      if ( _.isUndefined(this.list[hook_name]) ) {
+        this.list[hook_name] = { queue: [] };
+      }
+      var index = this.list[hook_name].queue.push(callback) - 1;
+
+      return {
+        remove: function() {
+          delete this.list[hook_name].queue[index];
+		}
+	  };
+    },
+
+    trigger: function(hook_name, data) {
+      if(_.isUndefined(this.list[hook_name]) || this.list[hook_name].queue.length == 0) {
+        return;
+      }
+
+      this.list[hook_name].queue.forEach(function(callback) {
+        callback(data || null);
+      });
+    }
+  }
+});
